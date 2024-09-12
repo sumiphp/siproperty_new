61b71d228d0a45bdabb3ce71af0c40e637991a74
Sumila <sumila.c@gmail.com>
2024-09-12 15:43:00 +0530

    messages

--- siproperty_new/application/third_party/geoip-api-php-master/timezone/test_timezone.php
+++
@@ -1,4 +0,0 @@
-<?php
-require("../src/timezone.php");
-print get_time_zone("CA", "ON");
-?>
--- siproperty_new/application/views/admin/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="localhost";
-$user="root";
-$pass="";
-$db="siproperty";
-$con=mysqli_connect($host,$user,$pass,$db);
-mysqli_select_db($con,$db) or die('cannot onnect');
-$redirectpath='http://localhost/siproperty11/';
-?>
